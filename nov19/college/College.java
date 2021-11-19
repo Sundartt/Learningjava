2421a7b340cc5e4f5daf1d2748f607bd1c937f7d
Sundartt <94391316+Sundartt@users.noreply.github.com>
2021-11-19 13:06:47 +0530

    Add files via upload

---
+++ nov19/college/College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+public class College {
+ 
+	public void collegeName() {
+		System.out.println("panimalar");
+	}
+	public void collegeCode() {
+		System.out.println("1022");
+	}
+	public void collegeRank() {
+		System.out.println("2ND");
+	}
+
+
+}
